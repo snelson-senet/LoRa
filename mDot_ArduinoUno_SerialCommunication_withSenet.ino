218c93d46fe8063c21561cff81f5784a03752f67
Shaun Nelson <snelson@senetco.com>
2016-02-04 17:16:24 -0500

    Working implementation Arduino/Multitech mDot

--- mDot_ArduinoUno_SerialCommunication_withSenet.ino
+++ mDot_ArduinoUno_SerialCommunication_withSenet.ino
@@ -19,6 +19,106 @@ int wait_time_to_join_network = 5000; // set delay to join the network
 int mDotResetPin = A0;  // super important! Make sure A0 is connected to mDot pin 5!
 String incomingString; // string coming from Serial
 String mDotString; // string coming from mDot
+bool   onNetwork = false;
+
+#define MDOT_BAUD_RATE  9600
+
+#define APP_EUI "00:25:0C:00:00:01:00:01"
+
+// Select device by uncommenting one of the following:
+// #define MINT
+// #define WALNUT
+// #define GRAPE
+
+#ifdef MINT   // mint on Anna's orange account 
+#define APP_KEY "CB:2C:63:CB:C8:AC:F2:31:FB:E8:BF:55:35:27:89:CC"
+#elif WALNUT  // walnut on Mike's orange account  
+#define APP_KEY "7E:74:B1:46:8D:5E:5E:0D:03:35:70:82:AC:E4:5B:04"
+#elif GRAPE    // grape on Mike's orange account  
+#define APP_KEY "9B:77:EE:F7:F7:A8:51:91:8C:00:A1:40:62:C6:52:A5"
+#else
+#warning APP_KEY for this device needs to be defined
+#define APP_KEY ""
+#endif
+
+#define APP_KEY_SIZE 47
+#define APP_EUI_SIZE 23
+
+  
+String sendToMDot(String& command)
+{
+  // String command = _command;
+  String output;
+  
+  // Dump command to Arduino Serial
+  Serial.print("\nSend to mDot: ");
+  Serial.println(command);
+
+  // Write command to mDot
+  mDotSerial.println(command);
+    
+  // Read mDot command result
+  output = mDotSerial.readString();
+
+  // Remove leading and trailing whitespace
+  output.trim();
+    
+  // Dump output to Arduino serial
+  Serial.print("Received from mDot: ");
+  Serial.println(output);
+
+  // Return mDot output
+  return output;
+}
+
+bool sendMDotCommand(String& command)
+{
+  int    ok;
+  String output;
+
+  // Send command to mDot
+  output = sendToMDot(command);
+
+  // Command was successful if mDot returned "OK"
+ return (output.indexOf("OK") != -1);
+}
+
+bool sendJoinCommand()
+{
+  bool   ok      = false;
+  bool   joined  = false;
+  String command = "AT+JOIN";
+  String output;
+   
+  for(int i = 0; (i < 100) && (joined == false); i++){  
+ 
+    // Dump command to Arduino Serial
+    Serial.println("JOINING");
+    mDotSerial.println(command);
+    
+    for(int j = 0; (j < 3) && (joined == false); j++){
+      // Read mDot command result
+      output = mDotSerial.readString();
+      
+      // Remove leading and trailing whitespace
+      output.trim();
+      
+      // Dump output to Arduino serial
+      Serial.print("\nJOIN received from mDot: ");
+      Serial.println(output);
+
+      if (output.indexOf("OK") != -1){
+        Serial.println("\nJOINED NETWORK!!");
+        joined = true;
+      }
+      else{
+        delay(1000);
+      }
+    }
+  }
+  return joined;
+}
+
 
 void setup() {
   
@@ -37,167 +137,78 @@ void setup() {
   delay(2000);  
   
   //configure the USB to the computer
-  Serial.begin(115200); 
+  Serial.begin(9600); 
   Serial.println("configured serial to home computer");
   //configure USB to the xbee/mDot
-  mDotSerial.begin(115200); //initialize the serial port. Set baud rate to 115200, same as mDot firmware
+  mDotSerial.begin(MDOT_BAUD_RATE); //initialize the serial port. Set baud rate to 115200, same as mDot firmware
   Serial.println("configured serial to mDot");
   //clear any existing outputs
   mDotSerial.flush();
   Serial.flush();
   //clear out whatever might have been in the receive buffer
   while (mDotSerial.available() != 0) mDotSerial.read();
-  
-  // get on the Senet network
-  get_on_the_network();
+
+  // Connect to network.
+  while (get_on_the_network() == false){
+    delay(1000);
+  }
 } // end of setup()
 
-void loop() {
-  // Communication : get AT commands from the Serial Monitor in the Arduino IDE ("home") and send to mDot
-  while (Serial.available()) {
-    incomingString = Serial.readString();
-    delay(10);
-    Serial.print("Home sent: ");
-    Serial.println(incomingString); 
-    mDotSerial.println(incomingString);
-    delay(10);
- } // end while get home data
-  delay(1000);
 
- // Communication : get mDot data 
- while (mDotSerial.available()) {
-    mDotString = mDotSerial.readString();
-    delay(10);
-    Serial.print("mDot response: ");
-    Serial.println(mDotString);
-    Serial.print("mDot check for ok: ");
-    Serial.println(check_for_ok(),DEC); 
- } // end while get mDot data
- 
- delay(1000);
-} // end of loop()
-
-// receive data from the mDot and see if there's an "OK" returned
-int check_for_ok (){
-  // Length (with one extra character for the null terminator)
-  int str_len = mDotString.length() + 1;
-  // Prepare the character array (the buffer) 
-  char char_array[str_len];
-  // Copy it over 
-  mDotString.toCharArray(char_array, str_len);
+bool get_on_the_network() {
   
-  for (int i = 0; i < str_len + 1; i++){
-    if(char_array[i] == 'O'){  // see if there's an "O" followed anywhere by a "K" since sometimes they're not consecutive.
-      for(int j=i; j < str_len + 1 - i; j++){
-        if(char_array[j] == 'K'){ return 1; } 
+  #define MDOT_CONFIG_COMMANDS 5
+  String mDotCfgCmd[MDOT_CONFIG_COMMANDS]= { "AT+PN=1",         // Senet is a public network
+                                             "AT+NI=0,"APP_EUI, // The Application Id this device is binding to 
+                                             "AT+NK=0,"APP_KEY, // The Application key for this specific device.
+                                             "AT+TXP=20",       // Transmit power
+                                             "AT+ADR 1"         // Turn on Adaptive Rate Control
+                                            }; 
+
+   String appKey = APP_KEY;
+   if(appKey.length() != APP_KEY_SIZE){
+      while(true){
+        Serial.println("APP_KEY is invalid!!\n");
+        delay(5000);
       }
-    // if(char_array[i+1] == 'K'){ return 1; } not working this way, I have no clue about what is happening
+   }
+
+   String appEui = APP_EUI;
+   if(appEui.length() != APP_EUI_SIZE){
+    while(true){
+        Serial.println("APP_EUI is invalid!!\n");
+        delay(5000);
+      }
+   }
+
+  // Set mDot LoRa configuration
+  for (int i = 0; i < MDOT_CONFIG_COMMANDS; i++){
+    while (sendMDotCommand(mDotCfgCmd[i]) == false){
+      delay(1000);
     }
   }
-  return 0;
-} // end check_for_ok()
 
-void get_on_the_network() {
-  // disable the echo; it causes problems: http://www.activexperts.com/sms-component/at/commands/?at=E0
-  mDotSerial.write("ATE0\r\n");
-  delay(blinkrate);
-  Serial.println("Sent ATEO");
-  mDotString = mDotSerial.readString(); // read string from mDot
-  delay(10);
-  Serial.print("mDot response: ");
-  Serial.println(mDotString); // print mDot data
-  Serial.print("checking for ok: ");
-  Serial.println(check_for_ok(),DEC);
-  Serial.println("");
+  // Display mDot configuration for debug
+  String statusCmd = "AT&V";
+  sendToMDot(statusCmd);
+ 
+  // Join network
+  return (sendJoinCommand());
+}
+
+void sendSensorData(String data)
+{
+  String command = "AT+SEND " + data;
+  sendMDotCommand(command);
+}
+    
+void loop() {
+  String measurement = "0123456789";
+  Serial.println("Transmit sensor data");
+  sendSensorData(measurement);
+  delay(30000);
   
-  // what does ATV0 do? 
-  mDotSerial.write("ATV0\r\n");
-  delay(blinkrate);
-  Serial.println("Sent ATVO");
-  mDotString = mDotSerial.readString(); // read string from mDot
-  delay(10);
-  Serial.print("mDot response: ");
-  Serial.println(mDotString); // print mDot data
-  Serial.print("checking for ok: ");
-  Serial.println(check_for_ok(),DEC);
-  Serial.println("");
-  
-  // set power to high = 20  
-  mDotSerial.println("AT+TXP=20");
-  delay(blinkrate);
-  Serial.println("Sent AT+TXP");
-  mDotString = mDotSerial.readString(); // read string from mDot
-  delay(10);
-  Serial.print("mDot response: ");
-  Serial.println(mDotString); // print mDot data
-  Serial.print("checking for ok: ");
-  Serial.println(check_for_ok(),DEC);
-  Serial.println("");
-  
-  // get the device ID 
-  mDotSerial.println("AT+DI");
-  delay(blinkrate);
-  Serial.println("Sent AT+DI");
-  mDotString = mDotSerial.readString(); // read string from mDot
-  delay(10);
-  Serial.print("mDot response: ");
-  Serial.println(mDotString); // print mDot data
-  Serial.println("");
-  
-  // Set a public network
-  mDotSerial.println("AT+PN=1");
-  delay(blinkrate);
-  Serial.println("Sent AT+PN");
-  mDotString = mDotSerial.readString(); // read string from mDot
-  delay(10);
-  Serial.print("mDot response: ");
-  Serial.println(mDotString); // print mDot data
-  Serial.print("checking for ok: ");
-  Serial.println(check_for_ok(),DEC);
-  Serial.println("");
-  
-  // send the Senet network identifier. This is your unique APP EUI from the Senet dashboard (amazingly the same for Mike & Anna)
-  // Mike's senet account: 00:25:0C:00:00:01:00:01
-  // Anna's senet account: 00:25:0C:00:00:01:00:01 
-  mDotSerial.println("AT+NI=0,00:25:0C:00:00:01:00:01");
-  delay(blinkrate);
-  Serial.println("Sent AT+NI");
-  mDotString = mDotSerial.readString(); // read string from mDot
-  Serial.print("mDot response: ");
-  Serial.println(mDotString); // print mDot data
-  Serial.print("checking for ok: ");
-  Serial.println(check_for_ok(),DEC);
-  Serial.println("");
-  
-  //send the app key that's unique to this mDot provided by Senet
-  //this is the unique APP KEY from your Senet dashboard (click on pen & paper)  
-  // mint on Anna's orange account    CB:2C:63:CB:C8:AC:F2:31:FB:E8:BF:55:35:27:89:CC
-  // walnut on Mike's orange account  7E:74:B1:46:8D:5E:5E:0D:03:35:70:82:AC:E4:5B:04
-  // grape on Mike's orange account   9B:77:EE:F7:F7:A8:51:91:8C:00:A1:40:62:C6:52:A5
-  mDotSerial.println("AT+NK=0,CB:2C:63:CB:C8:AC:F2:31:FB:E8:BF:55:35:27:89:CC");
-  delay(blinkrate);
-  Serial.println("Sent AT+NK");
-  mDotString = mDotSerial.readString(); // read string from mDot
-  delay(10);
-  Serial.print("mDot response: ");
-  Serial.println(mDotString); // print mDot data
-  Serial.print("checking for ok: ");
-  Serial.println(check_for_ok(),DEC);
-  Serial.println("");
-  
-  //Join the network
-  mDotSerial.println("AT+JOIN");
-  delay(blinkrate);
-  Serial.println("Sent AT+Join");
-  delay(wait_time_to_join_network); //wait to join the network
-  while(mDotSerial.available() > 0){ mDotString = mDotSerial.readString();} // read string from mDot
-  delay(10);
-  Serial.print("mDot response: ");
-  Serial.println(mDotString); // print mDot data
-  Serial.print("checking for ok: ");
-  Serial.println(check_for_ok(),DEC);
-  Serial.println("");
-  delay(1000);
-  Serial.println("Start sending data from the Serial Monitor to the mDot:");
-  
-} // end of get_on_the_network
+} // end of loop()
+
+
+
